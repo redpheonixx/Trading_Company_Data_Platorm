f3220963542bc79bcd8b3f18ac4eaf2e7b383899
unknown <amit220698@gmail.com>
2024-09-02 21:01:08 +0530

    script for pushing csv data to localstack s3 docker image completed

--- ingestion_csv.py
+++ ingestion_csv.py
@@ -1,12 +1,14 @@
 import csv
+import requests
+import boto3
 
-def read():
-    with open('./Data/trade_data.csv') as csvFile:
-        reader=csv.DictReader(csvFile)
-        for row in reader:
-            print(row['first_name'])
+class PushCsvData:
+    def __init__(self):
+        self.client=boto3.client("s3", endpoint_url='http://localhost:4566')
+    
+    def push(self):
+        file_path=r"C:\Users\singsina\Documents\DE\Trading_Platform_Data_Platorm\Data\trade_data.csv"
+        response = self.client.upload_file(file_path, 'csvdata-ldzone', 'data_01.csv')
+        print(response)
 
-
-def push():
-    s3.localhost.localstack.cloud
 
